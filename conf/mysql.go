9c712b7137db968705c67b6dc69f6ad2f1afab31
zhao liu <lz18738377974@163.com>
2023-07-08 15:19:27 +0800

    初始化DB全局实例

---
+++ conf/mysql.go
@@ -0,0 +1,43 @@
+package conf
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() error {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		C().MySQL.UserName,
+		C().MySQL.Password,
+		C().MySQL.Host,
+		C().MySQL.Port,
+		C().MySQL.Database,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	sqlDB, _ := db.DB()
+	// SetMaxIdleConns 用于设置连接池中空闲连接的最大数量。
+	sqlDB.SetMaxIdleConns(C().MySQL.MaxIdleConn)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(C().MySQL.MaxOpenConn)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
+	if err != nil {
+		return err
+	}
+
+	DB = db
+
+	return nil
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module github.com/sunflower10086/restful-api-demo
 go 1.19
 
 require (
-	github.com/BurntSushi/toml v1.3.2 // indirect
-	github.com/caarlos0/env/v6 v6.10.1 // indirect
+	github.com/BurntSushi/toml v1.3.2
+	github.com/caarlos0/env/v6 v6.10.1
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.2
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 )
